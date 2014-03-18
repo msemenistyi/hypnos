a0350fb87d7d0d4acf01b7fbdc36e737a1e154d5
msemenistyi <nikeiwe@gmail.com>
2014-03-18 19:18:28 +0200

    basic implementation

--- index.js
+++ index.js
@@ -4,10 +4,66 @@
 	} else if (typeof exports === 'object') {
 		module.exports = factory();
 	} else {
-		root.returnExports = factory();
+		root.Hypnos = factory();
 	}
 }(this, function () {
-	return function isSleeping(){
-		
+
+	function Hypnos(opts){
+
+		this.opts = {};
+		this.options(opts);
+
+		this._defaults = {
+			duration: 200,
+			error: 200
+		};
 	};
+
+	Hypnos.prototype.isSleeping = function(callback) {
+
+		if (typeof callback !== 'function'){
+			throw new Error('Hypnos module should be provided with callback function');
+		}
+
+		var startTime = new Date();
+
+		setTimeout(function(){
+			var now = new Date();
+			callback(now.valueOf() - startTime.valueOf() - options.duration > options.error);
+		}, options.duration);
+	};
+
+	Hypnos.prototype.options = function(options) {
+		var toBeReturned = false;
+		if (typeof options !== 'undefined'){
+			if (typeof options !== 'object'){
+				throw new Error('Hypnos: The argument passed to options method should be ' + 
+					'an object');
+			}
+			this.opts = options;
+		} else {
+			toBeReturned = true;
+		}
+
+		for (var i in this._defaults) {
+			if (this._defaults.hasOwnProperty(i)){
+				if (typeof this.opts[i] === 'undefined'){
+					this.opts[i] = this._defaults[i];
+				}
+			}
+		}
+
+		if (toBeReturned){
+			var opts = {};
+			for (var i in this.opts) if (this.opts.hasOwnProperty(i)){
+				opts[i] = this.opts[i];
+			}
+			return opts;
+		}
+
+
+	};
+
+	return Hypnos;
+
 }));
