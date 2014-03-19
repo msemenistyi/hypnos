fb425d937275e230e0a7066f3f07a01701ffce25
msemenistyi <nikeiwe@gmail.com>
2014-03-19 17:14:16 +0200

    initial commit

--- Makefile
+++
@@ -1,4 +0,0 @@
-all: min
-
-min:
-	uglifyjs ./lib/hypnos.js -o ./lib/hypnos.min.js -m
---
+++ app.js
@@ -0,0 +1,62 @@
+(function(doc, root){
+	var hypnos = new Hypnos();
+
+	var $ = Document.prototype.querySelector.bind(document);
+
+	var $el = {
+		screen: $('#screen'),
+		text: $('#cb-text'),
+		countdown: $('#cb-countdown'),
+		button: $('#sb-button'),
+		startBlock: $('#start-block') ,
+		countdownBlock: $('#countdown-block') 
+	}
+
+	var interval;
+	var result = false;
+
+	bindListeners();
+
+	function startCountdown(){
+		$el.startBlock.style.display = 'none';
+		$el.countdownBlock.style.display = 'block';
+		var seconds = 1;
+
+		$el.countdown.innerHTML = seconds;
+		interval = setInterval(function(){
+			seconds--;
+			$el.countdown.innerHTML = seconds;
+			if (!seconds) {
+				stopCountdown();
+			}
+		}, 1000);
+	}
+
+	function stopCountdown(){
+
+		clearInterval(interval);
+		hypnos.isSleeping(function(isSleeping){
+			result = isSleeping;
+			changeBackground();
+		});
+
+		$el.startBlock.style.display = 'block';
+		$el.countdownBlock.style.display = 'none';
+
+	}
+
+	function changeBackground () {
+		if (result){
+			$el.screen.className = 'night';
+		} else {
+			$el.screen.className = 'day';
+		}
+	}
+
+	function bindListeners(){
+		$el.button.addEventListener('click', function(event){
+			startCountdown();
+		}, true);
+	}
+
+}(document, window));
